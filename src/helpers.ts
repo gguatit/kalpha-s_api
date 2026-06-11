9c2ada71fd4564a8948b39182e6f5f026e66b960
gguatit <kako34133413@gmail.com>
2026-06-11 14:28:39 +0900

    feat: Dead Drop API 메시지 최대 길이 2000자에서 10000자로 증가

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,6 @@
 import type { Env } from './types';
 
-export const MAX_MESSAGE_LENGTH = 2000;
+export const MAX_MESSAGE_LENGTH = 10000;
 export const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 export const DEFAULT_ALLOWED_ORIGINS = ['https://kalpha.mmv.kr'] as const;
